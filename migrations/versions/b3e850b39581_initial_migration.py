86ac0e6fd730d35db822822ad2906d358560f715
bwjson <bwjson27@gmail.com>
2024-10-07 14:13:55 +0500

    final eeeehhehhe

--- migrations/versions/b3e850b39581_initial_migration.py
+++ migrations/versions/b3e850b39581_initial_migration.py
@@ -43,10 +43,12 @@ def upgrade() -> None:
     sa.Column('email', sa.String(), nullable=False),
     sa.Column('username', sa.String(), nullable=False),
     sa.Column('registered_at', sa.TIMESTAMP(), nullable=True),
-    sa.Column('hashed_password', sa.String(length=1024), nullable=False),
+    sa.Column('role_id', sa.Integer(), nullable=True),
+    sa.Column('hashed_password', sa.String(), nullable=False),
     sa.Column('is_active', sa.Boolean(), nullable=False),
     sa.Column('is_superuser', sa.Boolean(), nullable=False),
     sa.Column('is_verified', sa.Boolean(), nullable=False),
+    sa.ForeignKeyConstraint(['role_id'], ['role.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
