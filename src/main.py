77adc30803736efd919ad54358c160b1134a4a19
bwjson <bwjson27@gmail.com>
2024-10-07 12:26:15 +0500

    last

--- src/main.py
+++ src/main.py
@@ -1,33 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
+
 from src.auth.base_config import auth_backend, fastapi_users
-from src.auth.schemas import UserRead, UserCreate
+from src.auth.schemas import UserCreate, UserRead
+from src.config import REDIS_HOST, REDIS_PORT
 from src.operations.router import router as router_operation
 from src.tasks.router import router as router_tasks
 from src.pages.router import router as router_pages
 from src.chat.router import router as router_chat
-from contextlib import asynccontextmanager
-from fastapi.middleware.cors import CORSMiddleware
-from src.config import REDIS_HOST, REDIS_PORT
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    redis = aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}", encoding="utf8", decode_responses=True)
-    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
-    yield
-    await redis.close()
-
 
 app = FastAPI(
-    title="Trading App",
-    lifespan=lifespan
+    title="Trading App"
 )
 
-app.mount("/static", StaticFiles(directory="src/static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
@@ -47,8 +37,7 @@ app.include_router(router_pages)
 app.include_router(router_chat)
 
 origins = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
@@ -59,3 +48,9 @@ app.add_middleware(
     allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
                    "Authorization"],
 )
+
+
+@app.on_event("startup")
+async def startup_event():
+    redis = aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
